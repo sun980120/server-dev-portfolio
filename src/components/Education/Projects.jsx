53be3e0a2ec969679159f2c59308f1ae04391be4
WooSungHong <ws.hong@intin.kr>
2024-06-18 08:59:44 +0900

    내용수정

--- src/components/Education/Projects.jsx
+++ src/components/Education/Projects.jsx
@@ -20,7 +20,7 @@ const Projects = () => {
             period: '2024.02 - In Progress',
             title: 'SBOM 시스템 구축',
             company: '주식회사 인트인',
-            role: '단독개발, 웹 개발 및 배포 / 서버 개발 및 배포/ 서버 구축',
+            role: '단독개발, 웹 개발 및 유지보수 / 서버 개발 및 유지보수 / 웹&서버 배포 / 서버 구축 및 운영',
             techStack: 'React, Javascript, Spring Boot, Java, JPA, QueryDSL, MariaDB',
             keyTech: '싱글 페이지 애플리케이션(SPA) 구축, RESTful API 통신, SBOM 문서관리, OSS(OpenSource Software) 관리, 취약점 관리, 검색관리'
         },
@@ -28,7 +28,7 @@ const Projects = () => {
             period: '2023.11 - 2024.01',
             title: '정자분석기 2.1',
             company: '주식회사 인트인',
-            role: '단독개발, 서버 개발 및 배포/ 서버 운영',
+            role: '단독개발, 서버 개발 및 유지보수 / 서버 배포/ AWS 서버 운영',
             techStack: 'Spring Boot, Java, JPA, QueryDSL, Apple-OAuth, MariaDB',
             keyTech: 'RESTful API 통신 및 문서 작성, 버그 수정, 신규요구사항 기능 추가'
         },
@@ -36,7 +36,7 @@ const Projects = () => {
             period: '2023.11 - 2023.11',
             title: 'Multi-DTx 2차년도',
             company: '주식회사 인트인',
-            role: '팀 개발(2명, 기여도 30%), 코드 리뷰 / Jenkins 연동',
+            role: '팀 개발(2명, 기여도 30%), 코드 리뷰 / Git Repository 관리 / Jenkins 자동화 빌드',
             techStack: 'Spring Boot, Java, QueryDSL, MariaDB',
             keyTech: 'RESTful API 통신 문서 작성, 코드 리뷰'
         },
@@ -52,7 +52,7 @@ const Projects = () => {
             period: '2023.06 - 2023.08',
             title: '삼성생명 TheHealth X 인트인 콜라보',
             company: '주식회사 인트인',
-            role: '팀 개발(2명, 기여도 80%), 웹 배포 / 서버 개발 및 배포/ 서버 구축 및 운영',
+            role: '팀 개발(2명, 기여도 80%), 서버 개발 및 유지보수 / 웹&서버 배포 / AWS 서버 구축 및 운영',
             techStack: 'Spring Boot, Java, JPA, MariaDB',
             keyTech: 'NICE 본인인증, 코드 리뷰, 서버 구축 및 운영'
         },
@@ -60,7 +60,7 @@ const Projects = () => {
             period: '2023.01 - 2023.09',
             title: '정자분석기 2.0',
             company: '주식회사 인트인',
-            role: '팀 개발(2명, 기여도 60%), 서버 개발 및 배포/ 서버 구축 및 운영',
+            role: '팀 개발(2명, 기여도 60%), 서버 개발 및 유지보수 / 서버 배포 / AWS 서버 구축 및 운영',
             techStack: 'Spring Boot, Java, JPA, QueryDSL, JWT, MariaDB',
             keyTech: 'JWT 암호화 토큰 관리, 기존 사용자 마이그레이션, 신규 요구사항 적용'
         },
@@ -68,7 +68,7 @@ const Projects = () => {
             period: '2022.11 - 2023.02',
             title: 'Multi-DTx 1차년도',
             company: '주식회사 인트인',
-            role: '팀 개발(2명, 기여도 70%), 서버 개발 및 배포/ 서버 구축 및 운영',
+            role: '팀 개발(2명, 기여도 70%), 서버 개발 및 유지보수 / 서버 배포 / AWS 서버 구축 및 운영',
             techStack: 'NodeJS, Typescript, TypeORM, JWT, MariaDB',
             keyTech: 'HL7 연동, 서버 구축 및 운영'
         },
@@ -76,7 +76,7 @@ const Projects = () => {
             period: '2022.09 - 2023.05',
             title: '통합관리자웹',
             company: '주식회사 인트인',
-            role: '팀 개발(2명, 기여도 80%), 웹 배포 / 서버 개발 및 배포/ 서버 구축 및 운영',
+            role: '팀 개발(2명, 기여도 80%), 서버 개발 및 유지보수 / 웹&서버 배포 / AWS 서버 구축 및 운영',
             techStack: 'NodeJS, Typescript, TypeORM, JWT, MariaDB',
             keyTech: '관리자 관리, B2B 서비스'
         },
